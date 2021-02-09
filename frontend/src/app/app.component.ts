7a84680b9d8fad1fc34c58946ad51df32b68ea9c
fofware <fofware@gmail.com>
2021-02-09 14:53:43 -0300

    Arrancando con el frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Administracion';
+  menu = [
+    {
+      title: 'Configuración',
+      icon: 'fas fa-cogs fa-3x',
+      permiso: 'admin,modrator',
+      target: '_blank',
+      rel: 'noopener',
+      href: 'https://angular.io/tutorial'
+    },
+    {
+      title: 'Personas',
+      icon: 'fas fa-tasks fa-3x',
+      permiso: 'admin,modrator',
+      target: '_blank',
+      rel: 'noopener',
+      href: 'https://angular.io/tutorial'
+    },
+    {
+      title: 'Compras',
+      icon: 'fas fa-tasks fa-3x',
+      permiso: 'admin,modrator',
+      target: '_blank',
+      rel: 'noopener',
+      href: 'https://angular.io/tutorial'
+    },
+    {
+      title: 'Ventas',
+      icon: 'fas fa-tasks fa-3x',
+      permiso: 'modrator',
+      target: '_blank',
+      rel: 'noopener',
+      href: 'https://angular.io/tutorial'
+    },
+    {
+      title: 'Algo asdf asdf asdf asdf asd adsf ',
+      icon: 'fas fa-tasks fa-3x',
+      permiso: 'modrator',
+      target: '_blank',
+      rel: 'noopener',
+      href: 'https://angular.io/tutorial'
+    }
+   ];
 }
